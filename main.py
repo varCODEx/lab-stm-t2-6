c21130987487987dd34178b9d7b800464e83d5c1
varCODEx <borys.kovba@gmail.com>
2021-04-22 11:40:28 +0300

    complete gauss method

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from warnings import warn
+
 def subtract_row(r1, r2):
     return [r1[i] - r2[i] for i in range(len(r1))]
 
@@ -7,8 +9,8 @@ def backward_substitution(A, b):
     x = [None for i in range(len(A))]
     for i in reversed(range(len(A))):
         subtrahend = 0
-        for j in range(i, len(A)-1):
-            subtrahend += x[j+1] * A[i][j+1]
+        for j in range(i, len(A) - 1):
+            subtrahend += x[j + 1] * A[i][j + 1]
         x[i] = (b[i] - subtrahend) / A[i][i]
 
     return x
@@ -20,16 +22,21 @@ def gauss_method(A, b):
     # M = [M|b]
     M = [None for i in range(n)]
     for i in range(n):
-        M[i] = [l for l in A[i]]+[b[i]]
+        M[i] = [l for l in A[i]] + [b[i]]
 
     for i in range(n):
 
-        if M[i][i] != 0:
-            for j in range(i + 1, n):
-                M[j] = subtract_row(M[j], [elem * (M[j][i] / M[i][i]) for elem in M[i]])
-        else:
-            raise Exception("not implemented yet")
 
+        if M[i][i] == 0:
+            # pivot around the biggest (absolute) element in a row
+            M[i:] = sorted(M[i:], reverse=True, key=lambda row: abs(row[i]))
+            if M[i][i] == 0:
+                # if all elements below the diagonal are also zero, the matrix is singular
+                warn("The result of the gauss method is indecisive (either no or infinite solutions)")
+                return [None for i in range(n)]
+
+        for j in range(i + 1, n):
+            M[j] = subtract_row(M[j], [elem * (M[j][i] / M[i][i]) for elem in M[i]])
 
     # A_, b_ from [A|b]
     A_ = [el[:-1] for el in M]
@@ -49,5 +56,11 @@ A = [[1, 1, 1],
      [2, 4, 1]]
 
 b = [3, 4, 5]
+
+# A = [[1, 1, 1],
+#      [1, 1, 2],
+#      [2, 2, 1]]
+#
+# b = [3, 4, 5]
 
 print(gauss_method(A, b))
